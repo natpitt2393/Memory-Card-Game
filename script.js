c6f0a6a44aca51382bc71f1a0ad627c4036f3b5b
Nat Epstein <nathaniel.epstein@gmail.com>
2022-05-02 10:09:38 -0400

    added functionality so that cards cannot flip back over if they match, but if they do not, timer is set so that they flip back to backside

--- script.js
+++ script.js
@@ -7,9 +7,31 @@ let firstCard, secondCard;
 
 
 function flipCard() {
-    console.log('I was clicked!');
-    console.log(this);
-    this.classList.toggle('flip')
+    this.classList.add('flip');
+
+if (!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+} else {
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    //do cards match?
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        //they match!
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+    } else {
+        //they don't match
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+        }, 1500);
+    }
+ }
 }
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
